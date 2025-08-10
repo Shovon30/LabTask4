211315cf9c3ba6ace420b8a0f4d24ee6e6491e56
Shovon <dasshovon30@gmail.com>
2025-08-10 14:38:23 +0600

    Add files via upload

---
+++ class04.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class class04 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        // Taking three numbers as input
+        System.out.print("Enter first number: ");
+        int num1 = input.nextInt();
+
+        System.out.print("Enter second number: ");
+        int num2 = input.nextInt();
+
+        System.out.print("Enter third number: ");
+        int num3 = input.nextInt();
+
+        // Checking the largest number
+        if (num1 >= num2 && num1 >= num3) {
+            System.out.println("The largest number is: " + num1);
+        } else if (num2 >= num1 && num2 >= num3) {
+            System.out.println("The largest number is: " + num2);
+        } else {
+            System.out.println("The largest number is: " + num3);
+        }
+
+        input.close();
+    }
+}
